f0dfea44701a629b6ad323adeaba07e6f3953f36
ajrominger <ajrominger@gmail.com>
2021-08-25 14:00:04 -0400

    wrote out simulation routines

--- inst/sim.R
+++ inst/sim.R
@@ -1,13 +1,14 @@
-ntbSimViz(fname = 'first_sim.mp4', J = 6^2, m = 0.5, nu = 0.1, Sm = 20,
+ntbSimViz(fname = 'inst/first_sim.mp4', J = 6^2, m = 0.5, nu = 0.1, Sm = 20,
           SADm = pika::sad(model = 'stick', par = 0.99),
           nstep = 10, init = NULL,
-          pal = function(n) sample(viridis::viridis(n)))
+          pal = function(n) sample(viridis::viridis(n)), interval = 0.5)
 
 
 
 
-mean(rowSums(matrix(sample(1:6, 2 * 10000, replace = TRUE), ncol = 2)) <= 4)
+ntbSimViz(fname = 'inst/long_sim.mp4', J = 6^2, m = 0.5, nu = 0.1, Sm = 20,
+          SADm = pika::sad(model = 'stick', par = 0.99),
+          nstep = 500, init = NULL,
+          pal = function(n) sample(viridis::viridis(n)), interval = 0.1)
 
-# 1 and 1
-# 1 and 2
-# 2 and 1
+
